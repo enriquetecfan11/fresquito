1d4d41b64ba53f483175dfde157ca88647475e5c
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-03-08 21:16:44 +0100

    Minor Changes

--- main.py
+++ main.py
@@ -138,6 +138,11 @@ def main():
 
         # Scrape weather data
         pelmorex_list = town_index['pelmorex_id'].tolist()
+        
+        # Check for what item in the list are
+        for i, pelmorex_id in enumerate(pelmorex_list):
+            print(f"Loading data for location {i+1}/{len(pelmorex_list)}")
+            
         meteo_data = scrape_weather_data(pelmorex_list[:NUM_LOCATIONS])  # Limit to user-specified number
         meteo_df = pd.DataFrame(meteo_data)
 
